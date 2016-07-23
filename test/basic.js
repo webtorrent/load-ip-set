a321184b9cf3bb668b796dd65be81f1e563bf6b5
Jota De Picas <jotadepicas@gmail.com>
2016-07-23 15:54:53 -0300

    updated regex to support both ranges and single IPs, with or without a string description

--- test/basic.js
+++ test/basic.js
@@ -28,15 +28,23 @@ function checkList (t, ipSet) {
   t.ok(ipSet.contains('5.6.7.0'))
   t.ok(ipSet.contains('5.6.7.128'))
   t.ok(ipSet.contains('5.6.7.255'))
+  t.ok(ipSet.contains('192.168.1.1'))
+  t.ok(ipSet.contains('192.168.1.230'))
+  t.ok(ipSet.contains('192.168.1.100'))
+  t.ok(!ipSet.contains('192.168.1.231'))
+  t.ok(ipSet.contains('192.168.1.240'))
+  t.ok(!ipSet.contains('192.168.1.241'))
   t.ok(!ipSet.contains('1.1.1.1'))
   t.ok(!ipSet.contains('2.2.2.2'))
 }
 
 test('array of IP ranges', function (t) {
-  t.plan(9)
+  t.plan(15)
   loadIPSet([
     { start: '1.2.3.0', end: '1.2.3.255' },
-    { start: '5.6.7.0', end: '5.6.7.255' }
+    { start: '5.6.7.0', end: '5.6.7.255' },
+    { start: '192.168.1.1', end: '192.168.1.230' },
+    { start: '192.168.1.240', end: '192.168.1.240' }
   ], function (err, ipSet) {
     if (err) throw err
     checkList(t, ipSet)
@@ -44,7 +52,7 @@ test('array of IP ranges', function (t) {
 })
 
 test('http url', function (t) {
-  t.plan(9)
+  t.plan(15)
   var server = http.createServer(function (req, res) {
     fs.createReadStream(path.join(__dirname, 'list.txt'))
       .pipe(res)
@@ -60,7 +68,7 @@ test('http url', function (t) {
 })
 
 test('http url (with custom user agent)', function (t) {
-  t.plan(10)
+  t.plan(16)
   var server = http.createServer(function (req, res) {
     t.equal(req.headers['user-agent'], 'WebTorrent (http://webtorrent.io)')
     fs.createReadStream(path.join(__dirname, 'list.txt'))
@@ -79,7 +87,7 @@ test('http url (with custom user agent)', function (t) {
 })
 
 test('http url with gzip encoding', function (t) {
-  t.plan(9)
+  t.plan(15)
   var server = http.createServer(function (req, res) {
     res.setHeader('content-encoding', 'gzip')
     fs.createReadStream(path.join(__dirname, 'list.txt'))
@@ -97,7 +105,7 @@ test('http url with gzip encoding', function (t) {
 })
 
 test('http url with deflate encoding', function (t) {
-  t.plan(9)
+  t.plan(15)
   var server = http.createServer(function (req, res) {
     res.setHeader('content-encoding', 'deflate')
     fs.createReadStream(path.join(__dirname, 'list.txt'))
@@ -115,7 +123,7 @@ test('http url with deflate encoding', function (t) {
 })
 
 test('fs path', function (t) {
-  t.plan(9)
+  t.plan(15)
   loadIPSet(path.join(__dirname, 'list.txt'), function (err, ipSet) {
     if (err) throw err
     checkList(t, ipSet)
@@ -123,7 +131,7 @@ test('fs path', function (t) {
 })
 
 test('fs path with gzip', function (t) {
-  t.plan(9)
+  t.plan(15)
   loadIPSet(path.join(__dirname, 'list.txt.gz'), function (err, ipSet) {
     if (err) throw err
     checkList(t, ipSet)
